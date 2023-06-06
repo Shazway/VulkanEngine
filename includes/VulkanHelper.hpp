0b12dca3ea044b0fd3d786e3daa6427e49138fd1
tmoragli <tmoragli@student.42.fr>
2023-06-06 16:18:24 +0200

    Slight refactor, added debug_utils_extension to the extensions when debugging is active

--- includes/VulkanHelper.hpp
+++ includes/VulkanHelper.hpp
@@ -66,13 +66,25 @@ class VulkanHelper
 			return true;
 		}
 
+		std::vector<const char*> getRequiredExtensions() {
+			uint32_t glfwExtensionCount = 0;
+			const char** glfwExtensions;
+			glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+			std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+			if (VALIDATIONLAYER)
+				extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+			return extensions;
+		}
+
 		void createInstance() {
 			if (VALIDATIONLAYER && !checkValidationLayersupport())
 				throw std::runtime_error("Validation layers requested but not available!");
 			VkApplicationInfo	appInfo {};
 			VkInstanceCreateInfo	createInfo {};
-			uint32_t	glfwExtensionCount = 0;
-			const char	**glfWextensions;
+			std::vector<const char *>	extensions = getRequiredExtensions();
 
 			appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 			appInfo.pApplicationName = "Hello Triangle";
@@ -81,12 +93,18 @@ class VulkanHelper
 			appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 			appInfo.apiVersion = VK_API_VERSION_1_0;
 
-			createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 			createInfo.pApplicationInfo = &appInfo;
-			glfWextensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-			createInfo.enabledExtensionCount = glfwExtensionCount;
-			createInfo.ppEnabledExtensionNames = glfWextensions;
-			createInfo.enabledLayerCount = 0;
+			createInfo.enabledExtensionCount = extensions.size();
+			createInfo.ppEnabledExtensionNames = extensions.data();
+
+			if (VALIDATIONLAYER)
+			{
+				createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+				createInfo.ppEnabledLayerNames = validationLayers.data();
+				createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+			}
+			else
+				createInfo.enabledLayerCount = 0;
 
 			VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
 			if (result != VK_SUCCESS)
@@ -94,17 +112,18 @@ class VulkanHelper
 			else if (result == VK_SUCCESS)
 				std::cout << "Successfully created instance" << std::endl;
 
-			uint32_t	extensionsCount = 0;
-			vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
-			std::vector<VkExtensionProperties>	extensions(extensionsCount);
-			vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, extensions.data());
-			std::cout << "Available instance extensions:" << std::endl;
 
-			for (std::vector<VkExtensionProperties>::iterator it = extensions.begin(); it != extensions.end(); it++)
+			uint32_t	instanceExtensionsCount = 0;
+			vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionsCount, nullptr);
+			std::vector<VkExtensionProperties>	instanceExtensions(instanceExtensionsCount);
+			vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionsCount, instanceExtensions.data());
+
+			std::cout << "Available instance extensions: " + instanceExtensionsCount << std::endl;
+			for (std::vector<VkExtensionProperties>::iterator it = instanceExtensions.begin(); it != instanceExtensions.end(); it++)
 				std::cout << '\t' << C_BLUE << (*it).extensionName << C_END << std::endl;
 			std::cout << C_WHITE << "GLFW extensions: " << C_END << std::endl;
-			for (uint32_t i = 0; i < glfwExtensionCount; i++)
-				std::cout << "\t" << C_GREEN << glfWextensions[i] << C_END << std::endl;
+			for (std::vector<const char *>::const_iterator cit = extensions.begin(); cit != extensions.end(); cit++)
+				std::cout << "\t" << C_GREEN << (*cit) << C_END << std::endl;
 		}
 
 		void initVulkan() {
